bf0fb0231acaef4d3d23559f5a6600aef4a18aed
David Panal Cabrera <david.panal.cabrera@everis.com>
2021-06-25 13:42:28 +0200

    Refactor

--- __mocks__/elements.js
+++ __mocks__/elements.js
@@ -1,3 +1,49 @@
 module.exports = {
-    VEA_Calidad_Ambiental: '#contenido > div > div > div > div.cuerpoDiv.af_panelGroupLayout > div:nth-child(3) > div > div.elementoACentrar.af_panelGroupLayout > a',
+    page_title: 'Ventanilla Electrónica de la Administración de la Junta de Andalucía - Inicio',
+    vea_calidad_ambiental: '#contenido > div > div > div > div.cuerpoDiv.af_panelGroupLayout > div:nth-child(3) > div > div.elementoACentrar.af_panelGroupLayout > a',
+    aut_inst_tratamiento_residuos: '#tramites > div:nth-child(2) > div.elementoACentrar.af_panelGroupLayout > a',
+    nueva_solicitud: '#btnPresentacionSolicitud',
+    nif: '#nif',
+    nif_example: '00000000T',
+    iniciar_solicitud: '#botonLocalizador',
+    no_volver_a_mostrar: '#boton_ocultar_AdvCrearBorrador',
+    iniciar: '#j_id180 > div > div:nth-child(2) > div > span > a',
+    iframes: {
+        main: 'http://10.140.88.83/medioambiente/ctr_form-vea-ptw-externo/vea/inicio',
+        child: 'http://10.140.88.83/medioambiente/sira-form-AITR/datosPrevios'
+    },
+    menu: {
+        datos_previos: {
+            id:'app-menu-list-item.col.pl-pr-unset.ng-tns-c6-2.ng-star-inserted > a > div > label',
+            text: 'Datos previos'
+        },
+        datos_generales: {
+            id:'app-menu-list-item.col.pl-pr-unset.ng-tns-c6-3.ng-star-inserted > a > div > label',
+            text: 'Datos generales'
+        },
+        datos_especificos: {
+            id:'app-menu-list-item.col.pl-pr-unset.ng-tns-c6-4.ng-star-inserted > a > div > label',
+            text: 'Datos específicos'
+        },
+        documentacion_y_declaracion: {
+            id:'app-menu-list-item.col.pl-pr-unset.ng-tns-c6-5.ng-star-inserted > a > div > label',
+            text: 'Documentación y declaración'
+        }
+    },
+    nombre_del_centro_obligatorio: 'div.p-field.p-col.col-8.g-height-margin.pt-unset > div > p',
+    rnp: 'div.col-5.br-2.f-left > div.row.row-4-1 > div:nth-child(1) > p-checkbox > div > div.ui-chkbox-box.ui-widget.ui-corner-all.ui-state-default',
+    yes_no_1: '#mat-button-toggle-2-button',
+    datos_generales: {
+        nombre: {
+            id: 'div:nth-child(4) > div > div.p-field.col-4.g-height-margin > input',
+            warning: 'div.p-field.col-4.g-height-margin > div > p'
+        },
+        sexo: {
+            id: 'div:nth-child(4) > div > div:nth-child(4) > div > ng-select > div > div > div.ng-input > input[type=text]'
+        },
+        tipo_de_via: {
+            warning: 'div:nth-child(6) > div > div.p-field.p-col.col-3.g-height-margin > div > div > p'
+        }
+    },
+    body: 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(3)'
 }
--- __tests__/frontend.spec.js
+++ __tests__/frontend.spec.js
@@ -17,165 +17,146 @@ describe('Test header and title of the page', () => {
 
     test('Title of the page', async () => {
         const title = await page.title();
-        expect(title).toBe('Ventanilla Electrónica de la Administración de la Junta de Andalucía - Inicio');
+        expect(title).toBe(ELEMENTS.page_title);
     });
 
     test('Browse to the form', async () => {
-        await page.waitForSelector(ELEMENTS.VEA_Calidad_Ambiental);
-        await page.click(ELEMENTS.VEA_Calidad_Ambiental);
+        await page.waitForSelector(ELEMENTS.vea_calidad_ambiental);
+        await page.click(ELEMENTS.vea_calidad_ambiental);
         await page.waitForTimeout(5000);
-        await page.click('#tramites > div:nth-child(2) > div.elementoACentrar.af_panelGroupLayout > a');
+        await page.waitForSelector(ELEMENTS.aut_inst_tratamiento_residuos);
+        await page.click(ELEMENTS.aut_inst_tratamiento_residuos);
         await page.waitForTimeout(5000);
-        await page.click('#btnPresentacionSolicitud');
+        await page.click(ELEMENTS.nueva_solicitud);
         await page.waitForTimeout(5000);
-        await page.type('#nif', '00000000T');
+        await page.type(ELEMENTS.nif, ELEMENTS.nif_example);
         await page.waitForTimeout(5000);
-        await page.click('#botonLocalizador');
+        await page.click(ELEMENTS.iniciar_solicitud);
         await page.waitForTimeout(5000);
-        await page.click('#boton_ocultar_AdvCrearBorrador');
+        await page.click(ELEMENTS.no_volver_a_mostrar);
         await page.waitForTimeout(5000);
-        await page.click('#j_id180 > div > div:nth-child(2) > div > span > a');
-        await page.waitForTimeout(5000);
+        await page.click(ELEMENTS.iniciar);
+        await page.waitForTimeout(10000);
     });
 
-    test('Check the form exists', async () => {
+    test('Check the form exists (main iframe)', async () => {
         const childFrame = await page.mainFrame().childFrames()[0];
         const chidFrameUrl = childFrame._url;
-        expect(chidFrameUrl).toContain("http://10.140.88.83/medioambiente/ctr_form-vea-ptw-externo/vea/inicio");
+        expect(chidFrameUrl).toContain(ELEMENTS.iframes.main);
+    });
 
+    test('Check the form exists 2 (first childframe)', async () => {
+        const childFrame = await page.mainFrame().childFrames()[0];
         const subChildFrame = await childFrame.childFrames()[0];
         const subChidFrameUrl = subChildFrame._url;
-        expect(subChidFrameUrl).toContain("http://10.140.88.83/medioambiente/sira-form-AITR/datosPrevios");
+        expect(subChidFrameUrl).toContain(ELEMENTS.iframes.child);
     });
 
-    test('Check form main menu', async () => {
+    test('Check form main menu. Datos previos', async () => {
         const childFrame = await page.mainFrame().childFrames()[0];
-        const mainMenuItems = ['Datos previos', 'Datos generales', 'Datos específicos', 'Documentación y declaración'];
-        const mainMenuLength = await childFrame.evaluate('document.querySelectorAll("a > div > label")');
-        expect(mainMenuLength).toBe(4);
-        // expect([]).toHaveLength(4)
-        const mainMenuOption1 = await childFrame.evaluate('document.querySelectorAll("a > div > label")[0].innerText');
-        expect(mainMenuOption1).toBe(mainMenuItems[0]);
-        const mainMenuOption2 = await childFrame.evaluate('document.querySelectorAll("a > div > label")[1].innerText');
-        expect(mainMenuOption2).toBe(mainMenuItems[1]);
-        const mainMenuOption3 = await childFrame.evaluate('document.querySelectorAll("a > div > label")[2].innerText');
-        expect(mainMenuOption3).toBe(mainMenuItems[2]);
-        const mainMenuOption4 = await childFrame.evaluate('document.querySelectorAll("a > div > label")[3].innerText');
-        expect(mainMenuOption4).toBe(mainMenuItems[3]);
+        await childFrame.waitForSelector(ELEMENTS.menu.datos_previos.id);
+        let datosPrevios = await childFrame.$eval(ELEMENTS.menu.datos_previos.id, element => element.innerText);
+        expect(datosPrevios).toBe(ELEMENTS.menu.datos_previos.text);
+    });
+    test('Check form main menu. Datos generales', async () => {
+        const childFrame = await page.mainFrame().childFrames()[0];
+        await childFrame.waitForSelector(ELEMENTS.menu.datos_generales.id);
+        let datosPrevios = await childFrame.$eval(ELEMENTS.menu.datos_generales.id, element => element.innerText);
+        expect(datosPrevios).toBe(ELEMENTS.menu.datos_generales.text);
+    });
+    test('Check form main menu. Datos específicos', async () => {
+        const childFrame = await page.mainFrame().childFrames()[0];
+        await childFrame.waitForSelector(ELEMENTS.menu.datos_especificos.id);
+        let datosPrevios = await childFrame.$eval(ELEMENTS.menu.datos_especificos.id, element => element.innerText);
+        expect(datosPrevios).toBe(ELEMENTS.menu.datos_especificos.text);
+    });
+    test('Check form main menu. Documentación y declaración', async () => {
+        const childFrame = await page.mainFrame().childFrames()[0];
+        await childFrame.waitForSelector(ELEMENTS.menu.documentacion_y_declaracion.id);
+        let datosPrevios = await childFrame.$eval(ELEMENTS.menu.documentacion_y_declaracion.id, element => element.innerText);
+        expect(datosPrevios).toBe(ELEMENTS.menu.documentacion_y_declaracion.text);
     });
 
-    test('Check an element is not present', async () => {
-        const childFrame = await page.mainFrame().childFrames()[0];
-        await new Promise(resolve =>  setTimeout(resolve, 10000));
-        const subChildFrame = await childFrame.childFrames()[0];
-        const obligatorio = await childFrame.evaluate('document.querySelector("body > app-root > app-datos-previos > form > div > div > div.row-cabecera-2 > div.row.col-12.jc-space.p-m-unset > div.col-5.br-2.f-left > div.row.row-4-1 > div:nth-child(1) > div > p")');
-        expect(obligatorio).toBe(null);
-    });
+    // test('Check an element is not present', async () => {
+    //     const childFrame = await page.mainFrame().childFrames()[0];
+    //     const subChildFrame = await childFrame.childFrames()[0];
+    //     const obligatorio = await childFrame.evaluate('document.querySelector("body > app-root > app-datos-previos > form > div > div > div.row-cabecera-2 > div.row.col-12.jc-space.p-m-unset > div.col-5.br-2.f-left > div.row.row-4-1 > div:nth-child(1) > div > p")');
+    //     expect(obligatorio).toBe(null);
+    // });
 
     test('Check type of elements - Checkbox', async () => {
         const childFrame = await page.mainFrame().childFrames()[0];
         const subChildFrame = await childFrame.childFrames()[0];
-        await page.waitForTimeout(2000);
-        const id = 'body > app-root > app-datos-previos > form > div > div > div.row-cabecera-2 > div.row.col-12.jc-space.p-m-unset > div.col-5.br-2.f-left > div.row.row-4-1 > div:nth-child(1) > p-checkbox > div > div.ui-chkbox-box.ui-widget.ui-corner-all.ui-state-default';
+        await page.waitForTimeout(10000);
         const expectedClass = 'ui-chkbox-box ui-widget ui-corner-all ui-state-default';
-        const elementClass = await subChildFrame.$eval(id, element => element.className);
+        const elementClass = await subChildFrame.$eval(ELEMENTS.rnp, element => element.className);
         expect(elementClass).toBe(expectedClass);
-        await subChildFrame.click('body > app-root > app-datos-previos > form > div > div > div.row-cabecera-2 > div.row.col-12.jc-space.p-m-unset > div.col-5.br-2.f-left > div.row.row-4-1 > div:nth-child(1) > p-checkbox > div > div.ui-chkbox-box.ui-widget.ui-corner-all.ui-state-default');
-        await page.waitForTimeout(2000);
-        await subChildFrame.click('body > app-root > app-datos-previos > form > div > div > div.row-cabecera-2 > div.row.col-12.jc-space.p-m-unset > div.col-5.br-2.f-left > div.row.row-4-1 > div:nth-child(1) > p-checkbox > div > div.ui-chkbox-box.ui-widget.ui-corner-all.ui-state-default.ui-state-active');
-        await page.waitForTimeout(2000);
-        await subChildFrame.click('body > app-root > app-datos-previos > form > div > div > div.row-cabecera-2 > div.row.col-12.jc-space.p-m-unset > div.col-5.br-2.f-left > div.row.row-4-1 > div:nth-child(1) > p-checkbox > div > div.ui-chkbox-box.ui-widget.ui-corner-all.ui-state-default');
-        await page.waitForTimeout(2000);
-        await subChildFrame.click('body > app-root > app-datos-previos > form > div > div > div.row-cabecera-2 > div.row.col-12.jc-space.p-m-unset > div.col-5.br-2.f-left > div.row.row-4-1 > div:nth-child(1) > p-checkbox > div > div.ui-chkbox-box.ui-widget.ui-corner-all.ui-state-default.ui-state-active');
     });
 
     test('Check type of elements - YES/NO', async () => {
         const childFrame = await page.mainFrame().childFrames()[0];
         const subChildFrame = await childFrame.childFrames()[0];
-        await page.waitForTimeout(2000);
-        await subChildFrame.click('#mat-button-toggle-2-button');
-        const elementType = await subChildFrame.$eval('#mat-button-toggle-2-button', element => element.nodeName);
+        // await page.waitForTimeout(2000);
+        const elementType = await subChildFrame.$eval(ELEMENTS.yes_no_1, element => element.nodeName);
         expect(elementType).toBe("BUTTON");
-        await page.waitForTimeout(2000);
-        await subChildFrame.click('#mat-button-toggle-3-button');
-        await page.waitForTimeout(2000);
-        await subChildFrame.click('#mat-button-toggle-2-button');
-        await page.waitForTimeout(2000);
-        await subChildFrame.click('#mat-button-toggle-3-button');
     });
 
     test('Check type of elements - Text', async () => {
-        const datosGenerales = 'body > app-root > app-home-vea > mat-sidenav-container > mat-sidenav > div > mat-nav-list > app-menu-list-item.col.pl-pr-unset.ng-tns-c6-3.ng-star-inserted > a > div > label';
         const childFrame = await page.mainFrame().childFrames()[0];
-        await childFrame.click(datosGenerales);
+        await childFrame.click(ELEMENTS.menu.datos_generales.id);
         await page.waitForTimeout(5000);
         const subChildFrame = await childFrame.childFrames()[0];
-        const id = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(4) > div > div.p-field.col-4.g-height-margin > input';
-        await subChildFrame.type(id, 'test');
-        const elementType = await subChildFrame.$eval(id, element => element.type);
+        const elementType = await subChildFrame.$eval(ELEMENTS.datos_generales.nombre.id, element => element.type);
         expect(elementType).toBe("text");
     });
 
     test('Check type of elements - Select Combo', async () => {
-        const datosGenerales = 'body > app-root > app-home-vea > mat-sidenav-container > mat-sidenav > div > mat-nav-list > app-menu-list-item.col.pl-pr-unset.ng-tns-c6-3.ng-star-inserted > a > div > label';
         const childFrame = await page.mainFrame().childFrames()[0];
-        await childFrame.click(datosGenerales);
-        await page.waitForTimeout(10000);
+        await childFrame.click(ELEMENTS.menu.datos_generales.id);
+        await page.waitForTimeout(5000);
         const subChildFrame = await childFrame.childFrames()[0];
-        const id = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(4) > div > div:nth-child(4) > div > ng-select > div > div > div.ng-input > input[type=text]';
-        const elementType = await subChildFrame.$eval(id, element => element.getAttribute('role'));
+        const elementType = await subChildFrame.$eval(ELEMENTS.datos_generales.sexo.id, element => element.getAttribute('role'));
         expect(elementType).toBe("combobox");
     });
 
     test('Check elements - Attributes - Max Length', async () => {
-        const datosGenerales = 'body > app-root > app-home-vea > mat-sidenav-container > mat-sidenav > div > mat-nav-list > app-menu-list-item.col.pl-pr-unset.ng-tns-c6-3.ng-star-inserted > a > div > label';
         const childFrame = await page.mainFrame().childFrames()[0];
-        await childFrame.click(datosGenerales);
-        await page.waitForTimeout(10000);
+        await childFrame.click(ELEMENTS.menu.datos_generales.id);
+        await page.waitForTimeout(5000);
         const subChildFrame = await childFrame.childFrames()[0];
-        const id = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(4) > div > div.p-field.col-4.g-height-margin > input';
-        const elementType = await subChildFrame.$eval(id, element => element.maxLength);
+        const elementType = await subChildFrame.$eval(ELEMENTS.datos_generales.nombre.id, element => element.maxLength);
         expect(elementType).toBe(255);
     });
 
     test('Check elements - Attributes - Tooltip', async () => {
-        const datosGenerales = 'body > app-root > app-home-vea > mat-sidenav-container > mat-sidenav > div > mat-nav-list > app-menu-list-item.col.pl-pr-unset.ng-tns-c6-3.ng-star-inserted > a > div > label';
+
         const childFrame = await page.mainFrame().childFrames()[0];
-        await childFrame.click(datosGenerales);
-        await page.waitForTimeout(10000);
+        await childFrame.click(ELEMENTS.menu.datos_generales.id);
+        await page.waitForTimeout(5000);
         const subChildFrame = await childFrame.childFrames()[0];
-        const id = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(4) > div > div.p-field.col-4.g-height-margin > input';
-        const elementType = await subChildFrame.$eval(id, element => element.getAttribute('ng-reflect-message'));
+        const elementType = await subChildFrame.$eval(ELEMENTS.datos_generales.nombre.id, element => element.getAttribute('ng-reflect-message'));
         expect(elementType).toBe('Indique el nombre/razón social');
     });
 
     test('Check warning messages - Nombre correcto', async () => {
-        const datosGenerales = 'body > app-root > app-home-vea > mat-sidenav-container > mat-sidenav > div > mat-nav-list > app-menu-list-item.col.pl-pr-unset.ng-tns-c6-3.ng-star-inserted > a > div > label';
         const childFrame = await page.mainFrame().childFrames()[0];
-        await childFrame.click(datosGenerales);
-        await page.waitForTimeout(10000);
+        await childFrame.click(ELEMENTS.menu.datos_generales.id);
+        await page.waitForTimeout(5000);
         const subChildFrame = await childFrame.childFrames()[0];
-        const id = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(4) > div > div.p-field.col-4.g-height-margin > input';
-        await subChildFrame.type(id, '-');
-        const id2 = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(3)';
-        await subChildFrame.click(id2);
-        const warning = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(4) > div > div.p-field.col-4.g-height-margin > div > p';
-        let elementType = await subChildFrame.$eval(warning, element => element.innerText);
+        await subChildFrame.type(ELEMENTS.datos_generales.nombre.id, '-');
+        await subChildFrame.click(ELEMENTS.body);
+        let elementType = await subChildFrame.$eval(ELEMENTS.datos_generales.nombre.warning, element => element.innerText);
         expect(elementType).toBe('Introduce un nombre correcto');
     });
 
     test('Check warning messages - Dato Obligatorio', async () => {
-        const datosGenerales = 'body > app-root > app-home-vea > mat-sidenav-container > mat-sidenav > div > mat-nav-list > app-menu-list-item.col.pl-pr-unset.ng-tns-c6-3.ng-star-inserted > a > div > label';
         const childFrame = await page.mainFrame().childFrames()[0];
-        await childFrame.click(datosGenerales);
-        await page.waitForTimeout(10000);
+        await childFrame.click(ELEMENTS.menu.datos_generales.id);
+        await page.waitForTimeout(5000);
         const subChildFrame = await childFrame.childFrames()[0];
-        const id = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(4) > div > div.p-field.col-4.g-height-margin > input';
-        await subChildFrame.type(id, '-');
-        const id2 = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(3)';
-        await subChildFrame.click(id2);
-        const warning2 = 'body > app-root > datos-generales > form > div > div > div.form-datos > div:nth-child(6) > div > div.p-field.p-col.col-3.g-height-margin > div > div > p';
-        elementType = await subChildFrame.$eval(warning2, element => element.innerText);
+        await subChildFrame.type(ELEMENTS.datos_generales.nombre.id, '-');
+        await subChildFrame.click(ELEMENTS.body);
+        elementType = await subChildFrame.$eval(ELEMENTS.datos_generales.tipo_de_via.warning, element => element.innerText);
         expect(elementType).toBe('Obligatorio');
     });
     
